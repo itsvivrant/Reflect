cd7baffd6a1dc55863007b86ca1e9bbc48f02016
Vivian C <baongocche@gmail.com>
2021-09-06 13:17:44 -0700

    added the delete modal

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -24,5 +24,5 @@ def password_matches(form, field):
 
 
 class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired(), password_matches])
+    email = StringField('Email', validators=[DataRequired(), user_exists])
+    password = StringField('Password', validators=[DataRequired(), password_matches])
