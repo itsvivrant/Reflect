beb8919308269c6987aa36890c274741d1371148
Vivian C <baongocche@gmail.com>
2021-09-07 01:59:37 -0700

    changes

--- app/forms/entry_form.py
+++ app/forms/entry_form.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, ValidationError, Length, InputRequired
 
 class EntryForm(FlaskForm):
-    title = StringField('Title', validators=[Length(min=3, max=20, message="Title must be more than 3 and greater than 20 characters.")])
+    title = StringField('Title', validators=[Length(min=1, max=20, message="Title must be more than 0 and less than 20 characters.")])
     content = TextField('Entry', validators=[InputRequired(message="Don't forget to write something!")])
     strengths = StringField("Strengths", validators=[DataRequired(())])
     user_id = IntegerField('user_id', validators=[DataRequired(())])
